11ac4cccfbd41d3b0795082834e5280e3fd05536
clubaaron <emailclubaaron@gmail.com>
2017-09-08 19:39:46 -0400

    Retrieve Nicol Bolas, God-Pharoah from API and print out some values.

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 const mtg = require('mtgsdk');
 
 const app = express();
 
+// Set view engine.
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', function (req, res) {
 	mtg.card.where({ name: 'Nicol Bolas, God-Pharaoh'})
-	.then(result => {
-	    res.send(result);
+	.then(cards => {
+		// res.send(cards[0].name);
+	    res.render('index', { card: cards[0] });
 	});
  //  	res.send('Hello World!');
 });
